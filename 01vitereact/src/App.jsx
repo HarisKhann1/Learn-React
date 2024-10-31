5baa7b16bc7b478676e0bceb3d96e0b9dc7794ab
unknown <hariskhan3428@gmail.com>
2024-10-31 20:14:16 +0500

    know about react and variables

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import { Chai, Test } from './chai.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur react with vite | Haris Khan</h1>
+      <p>testing para</p>
+      <Chai />
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,14 @@
+function Chai(params) {
+    return(
+        <h3>Chai is ready.</h3>
+    )
+}
+
+function Test(params) {
+    const  name = 'Test Variable';
+    return(
+           <h3>Test is ready. {name}</h3>
+    )   
+   }
+
+export {Chai, Test};
