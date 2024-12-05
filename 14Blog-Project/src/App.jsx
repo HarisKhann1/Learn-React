03e5c89179d2151e898f878eb5668c7cbe39e5d6
unknown <hariskhan3428@gmail.com>
2024-12-05 20:43:09 +0500

    setup ENV and Appwrite

---
+++ 14Blog-Project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL) // "123"
+
+  return (
+    <>
+      <h1>Blog Project</h1>
+    </>
+  )
+}
+
+export default App
